770a1bd2ce7e0877a6a01d4630153e5ea3d3d552
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-06-03 16:56:56 +0100

    use item model

--- bin.js
+++ bin.js
@@ -10,7 +10,7 @@ const Item = require('./models/item')
 
 const _feed = hypercore(APP_ROOT, { createIfMissing: true, overwrite: false })
 
-function processCommand (feed) {
+function command (feed) {
   return yargs
     .command('add', 'add an item to your list', (yargs) => {
       yargs.positional('name', {
@@ -27,7 +27,10 @@ function processCommand (feed) {
     .command('get', 'get an item from your list', (argv) => {
       const index = argv.argv._[1]
       if (isNaN(index)) return callback(new Error(`${index} is not a number.`))
-      feed.get(index, { valueEncoding: 'json' }, callback)
+      feed.get(index, { valueEncoding: 'json' }, (err, item) => {
+        if (err) callback(err)
+        else callback(null, new Item(item))
+      })
     })
 
     .command('display', 'display your entire list', () => {
@@ -36,7 +39,7 @@ function processCommand (feed) {
       const items = []
       stream.on('data', (chunk) => {
         const item = JSON.parse(chunk.toString())
-        items.push(item)
+        items.push(new Item(item))
       }).on('end', () => {
         callback(null, items)
       })
@@ -55,9 +58,9 @@ function callback (err, res) {
 }
 
 _feed.on('ready', () => {
-  const yargsargs = processCommand(_feed)
+  const args = command(_feed)
 
-  if (!yargsargs._[0]) {
+  if (!args._[0]) {
     yargs.showHelp()
     _feed.close()
   }
