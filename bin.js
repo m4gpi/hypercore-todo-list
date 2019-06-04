cc1f9f955a089625c7f3e814af208725f5ff20d7
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-06-04 17:02:11 +0100

    simplify the CLI, extract logic

--- bin.js
+++ bin.js
@@ -1,4 +1,3 @@
-const hypercore = require('hypercore')
 const path = require('path')
 const os = require('os')
 const yargs = require('yargs')
@@ -8,67 +7,60 @@ const APP_NAME = require('./package.json').name
 const APP_ROOT = path.join(os.homedir(), `.${APP_NAME}`)
 const PUBLIC_KEY = 'a603500817d4f9dbbaa96b9421856156641833e361ee63d391d561a5375d9939'
 
-const Item = require('./models/item')
-const swarm = require('./swarm')
+function App () {
+  const HyperList = require('./index')
 
-const _feed = fs.existsSync(`${APP_ROOT}/secret_key`)
-  ? hypercore(APP_ROOT)
-  : hypercore(APP_ROOT, PUBLIC_KEY)
+  const hyperlist = new HyperList(APP_ROOT, PUBLIC_KEY)
 
-function command (feed) {
-  return yargs
-    .command('share', 'share your list', (argv) => {
-      swarm(feed)
-    })
+  hyperlist.feed((feed) => Command(feed))
 
-    .command('add', 'add an item to your list', (yargs) => {
-      yargs.positional('name', {
-        describe: 'give the item a name',
-        demandOption: true,
-        type: 'string'
+  function Command (feed) {
+    return yargs
+      .command('swarm', 'swarm to share your list', (argv) => {
+        hyperlist.swarm((swarm) => callback(null, swarm))
       })
-    }, (argv) => {
-      const { name } = argv
-      const item = new Item({ name })
-      feed.append(item.toString(), callback)
-    })
 
-    .command('get', 'get an item from your list', (argv) => {
-      const index = argv.argv._[1]
-      if (isNaN(index)) return callback(new Error(`${index} is not a number.`))
-      feed.get(index, { valueEncoding: 'json' }, (err, item) => {
-        if (err) callback(err)
-        else callback(null, new Item(item))
+      .command('add', 'add an item to your list', (yargs) => {
+        yargs.positional('name', {
+          demandOption: true,
+          type: 'string'
+        })
+      }, (argv) => {
+        const { name } = argv
+        hyperlist.add({ name }, callback)
       })
-    })
 
-    .command('display', 'display your entire list', () => {
-      var stream = feed.createReadStream({ start: 0, end: feed.length })
-
-      const items = []
-      stream.on('data', (chunk) => {
-        const item = JSON.parse(chunk.toString())
-        items.push(new Item(item))
-      }).on('end', () => {
-        callback(null, items)
+      .command('remove', 'remove an item from your list', (yargs) => {
+        yargs.positional('id', {
+          demandOption: true,
+          type: 'string'
+        })
+      }, (argv) => {
+        const { id } = argv
+        hyperlist.remove({ itemId: id }, callback)
       })
-    })
 
-    .argv
-}
+      .command('find', 'find an item from the list by its ID', (yargs) => {
+        yargs.positional('id', {
+          demandOption: true,
+          type: 'string'
+        })
+      }, (argv) => {
+        const { id } = argv
+        hyperlist.find(id, callback)
+      })
 
-function callback (err, res) {
-  if (err) throw err
-  console.log(res)
-}
+      .command('list', 'display your entire list', () => {
+        hyperlist.list(callback)
+      })
 
-_feed.on('ready', () => {
-  const args = command(_feed)
-
-  fs.existsSync(`${APP_ROOT}/secret_key`) ? null :  _feed.replicate()
-
-  if (!args._[0]) {
-    yargs.showHelp()
-    _feed.close()
+      .argv
   }
-})
+
+  function callback (err, res) {
+    if (err) throw err
+    console.log(res)
+  }
+}
+
+App()
