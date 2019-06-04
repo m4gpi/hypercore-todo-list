94107d00925fe4c566a2e73c458308b3d77e120e
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-06-04 17:01:39 +0100

    setup swarm as a separate module

--- swarm.js
+++ swarm.js
@@ -1,17 +1,30 @@
 const Discovery = require('hyperdiscovery')
 
-module.exports = function swarm (feed) {
-  const discovery = Discovery(feed)
-  console.log("Peer ID: ", discovery.id.toString('hex'))
+module.exports = function swarm (hyperlist, callback) {
+  callback = callback || noop
 
-  discovery.on('connection', (peer, type) => {
-    console.log("No of connections: ", discovery.totalConnections)
-    console.log("Peer connected: ", peer.id.toString('hex'))
+  hyperlist.getKey((err, key) => {
+    if (err) return callbackk(err)
 
-    peer.on('close', function () {
-      console.log('peer disconnected')
+    hyperlist.feed((feed) => {
+      const swarm = Discovery(feed, key)
+      swarm.on('connection', (peer, type) => {
+        console.log("Current number of connections: ", swarm.totalConnections)
+
+        var remoteKey = peer.id.toString('hex')
+        console.info(`${remoteKey} connected`)
+
+        hyperlist.replicate()
+        hyperlist.addConnection(remoteKey)
+
+        peer.on('close', function () {
+          console.log(`${remoteKey} dropped`)
+        })
+      })
+
+      callback(null, swarm)
     })
   })
-
-  return discovery
 }
+
+function noop () {}
